3c392597e3bc18e84f84cf1be125e0fbab407df4
matsu_friends <pikapikan0526@gmail.com>
2020-12-31 22:03:28 +0900

    Init

---
+++ Singleton.cs
@@ -0,0 +1,11 @@
+﻿public abstract class Singleton<T> where T : new() {
+    private static T sInstance;
+
+    public static T Instance {
+        get {
+            if (sInstance != null) return sInstance;
+            sInstance = new T();
+            return sInstance;
+        }
+    }
+}
---
+++ SingletonMono.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public abstract class SingletonMono<T> : MonoBehaviour where T : SingletonMono<T> {
+    private static T sInstance;
+
+    public static T Instance {
+        get {
+            if (sInstance != null) return sInstance;
+            var scene = SceneManager.GetActiveScene();
+
+            var componentsList = new List<T>();
+
+            foreach (var obj in scene.GetRootGameObjects()) {
+                var components = obj.GetComponentsInChildren<T>(true);
+                if (components.Length != 0) {
+                    componentsList.AddRange(components);
+                }
+            }
+
+            if (componentsList.Count      == 0) Debug.Log($"{typeof(T)}が見つかりません");
+            else if (componentsList.Count > 1) Debug.Log($"{typeof(T)}が複数存在します");
+            else sInstance = componentsList[0];
+            sInstance.OnInstanceMade();
+            return sInstance;
+        }
+    }
+
+    protected virtual void OnInstanceMade() { }
+}
